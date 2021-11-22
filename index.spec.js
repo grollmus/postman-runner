0f7792ff728e40f847bb74bdc87c787d2cde215d
Alexander Wiesinger <alexander.wiesinger@grollmus.de>
2021-11-22 16:57:41 +0100

    add tests for runner

--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,25 @@
 describe('postman-runner', () => {
-  it('should run collection multiple times', () => {
-    expect('').toBe('');
+  beforeEach(() => {
+    process.argv = ['', ''];
+  });
+
+  it('should fail when no path to collection was provided.', () => {
+    expect(() => require('./index.js')).toThrow('No path to collection provided! Use --collection');
+  });
+
+  it('should run collection multiple times.', () => {
+    jest.doMock('newman', () => {
+      return { run: jest.fn(() => {}) };
+    });
+    const newman = require('newman');
+
+    process.argv = ['test', '--collection="./sample-collections/TSTcloud.postman_collection.json"', '--runCount=30'];
+
+    require('./index.js');
+    expect(newman.run).toHaveBeenCalledTimes(30);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 });
