00920b2438882b3e5a4a069c2755effb4aa7c317
Alexander Wiesinger <alexander.wiesinger@grollmus.de>
2023-09-28 10:38:19 +0200

    do not enforce linebreak style

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,6 @@ module.exports = {
   },
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
   },
